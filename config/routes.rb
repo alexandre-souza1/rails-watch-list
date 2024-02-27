774952ac15f819bc23f620fa4b20e1e2c51bb565
Cleiston Amaral <c.alexandresouza@yahoo.com>
2024-02-26 23:23:31 -0300

    challenge 2 lewagon day 29

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,21 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  root to: "lists#index"
+
+  resources :lists do
+    resources :bookmarks
+  end
+
+  resources :bookmarks, only: :destroy
 
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+  # resources :bookmarks do
+  #   resources :list_id, only: [:show]
+  #   resources :movie_id, only: [:show]
+  # end
+  # resources :lists
+  # resources :movies
